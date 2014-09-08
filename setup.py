ceb570afac475f4b4c37e8b034684e0d9b4b5d91
Carlos Gonzalez <soulless_dead@hotmail.com>
2014-09-08 10:05:45 -0430

    Estructura de paquetes eggs policy inicial

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='fcv.genetica.policy',
+      version=version,
+      description="Paquete que aplica configuraciones personalizada para el sitio",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='fcv genetica animal plone luz policy',
+      author='Carlos Gonzalez',
+      author_email='soulless_dead@hotmail.com',
+      url='https://github.com/soullessdead/fcv.genetica.policy',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['fcv'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
