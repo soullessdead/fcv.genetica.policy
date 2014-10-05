fc328c5db09d15da447d746cc841a184d7230c79
Carlos Gonzalez <soulless_dead@hotmail.com>
2014-10-05 15:59:32 -0430

    creacion del tipo de contenido Products.CMFBibliographyAT utilizando setuphandler.py

--- fcv/genetica/policy/setuphandlers.py
+++ fcv/genetica/policy/setuphandlers.py
@@ -67,6 +67,16 @@ def create_defaults_contents(portal):
     obj.reindexObject('Title')
     logger.info("Renamed the {0} item".format(obj))
 
+    #Create Products.CMFBibliographyAT section
+    title = u'Bibliografias'
+    obj = api.content.create(type='BibliographyFolder', title=title, container=portal)
+    obj.setTitle(title)
+    obj.reindexObject('Title')
+    #obj_contrain_types = ['', '', '', '', '', '', '']
+    #constrain_types(obj, obj_contrain_types)
+    api.content.transition(obj,'publish')
+    logger.info("Created the {0} item".format(obj))
+    
     #Create FacultyStaffDirectory section
     title = u'Personal'
     obj = api.content.create(type='FSDFacultyStaffDirectory', title=title, container=portal)
